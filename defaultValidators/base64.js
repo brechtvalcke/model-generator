79629c71fc67664a3320da038509209b662cf0fc
BaeleM <marco.baele@gmail.com>
2017-10-07 14:37:05 +0200

    Base64 validator

--- defaultValidators/base64.js
+++ defaultValidators/base64.js
@@ -1,7 +1,7 @@
 module.exports = {
     Name : 'Base64',
     Validate : (value) => {
-        const base64Re = /smt/;
+        const base64Re = /^[a-zA-z0-9-_]*$/;
         if (!base64Re.test(value)) {
             throw Error('Property was not valid');
         }
