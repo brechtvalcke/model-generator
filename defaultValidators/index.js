7417b380656ad8f43da1355c5bf082adf1157d4e
BRECHT-PC\brecht <brecht.valcke@gentwebart.be>
2017-10-13 23:01:34 +0200

    validators fixed

--- defaultValidators/index.js
+++ defaultValidators/index.js
@@ -1,9 +1,33 @@
 let glob = require("glob");
 let DefaultValidators = [];
+let Validator;
+       
+Validator = require('./base64');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./dateTime');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./defaultPassword');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./email');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./lettersAndNumbers');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./LettersOnly');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./nationalRegistryNumberBE');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./numbersOnly');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./phoneNumber');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./required');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./string');
+DefaultValidators[Validator.Name] = Validator;
+Validator = require('./text');
+DefaultValidators[Validator.Name] = Validator;
 
-glob.sync('defaultValidators/!(index).js').forEach((route) => {
-        let Validator = require("../"+route);
-        DefaultValidators[Validator.Name] = Validator;
-});
+
+
 
 module.exports = DefaultValidators;
