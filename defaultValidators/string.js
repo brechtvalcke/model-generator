da3030576a9a6cb116f11f18bea1150c6932c037
VolckaertAchiel <achielvolckaert@msn.com>
2017-10-18 20:51:47 +0200

    updated validator to accept empty vals

--- defaultValidators/string.js
+++ defaultValidators/string.js
@@ -1,7 +1,7 @@
 module.exports = {
     Name : 'String',
     Validate : (value,propName) => {
-        const stringRe = /^(?:(?![×Þß÷þø])[-a-zA-ZÀ-ÿ.])$/;
+        const stringRe = /^(?:(?![×Þß÷þø])[-a-zA-ZÀ-ÿ.]|^)$/;
         if (!stringRe.test(value)) {
             throw Error(propName+' was not valid');
         }
